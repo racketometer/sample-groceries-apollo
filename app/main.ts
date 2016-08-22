f3c9c658cf81196cdfd2fab1ff579cd5d5dd2362
Bjørn <bjoern.soerensen@gmail.com>
2016-08-22 17:37:37 +0200

    Add angular2-apollo. Added types to silence tsc on builds.

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,20 @@
 import { nativeScriptBootstrap } from "nativescript-angular/application";
+import { APOLLO_PROVIDERS, defaultApolloClient } from "angular2-apollo";
+import ApolloClient, { createNetworkInterface } from "apollo-client";
 
 import { APP_ROUTER_PROVIDERS } from "./app.routes";
 import { AppComponent } from "./app.component";
 import { setStatusBarColors, BackendService, LoginService } from "./shared";
 
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface('http://localhost:8080'),
+})
+
 setStatusBarColors();
 nativeScriptBootstrap(AppComponent, [
   BackendService,
   LoginService,
+  APOLLO_PROVIDERS,
+  defaultApolloClient(client),
   APP_ROUTER_PROVIDERS
 ]);
