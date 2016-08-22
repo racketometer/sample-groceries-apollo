226fc6c8f124b47d61e234c9a175b41af0e97522
Bjørn <bjoern.soerensen@gmail.com>
2016-08-22 16:55:56 +0200

    Inital fork from NativeScript/sample-Groceries

---
+++ app/app.routes.ts
@@ -0,0 +1,16 @@
+import { RouterConfig } from "@angular/router";
+import { nsProvideRouter } from "nativescript-angular/router";
+
+import { LoginComponent } from "./login/login.component";
+import { GroceriesComponent } from "./groceries/groceries.component";
+import { AuthGuard } from "./auth.guard";
+
+export const routes: RouterConfig = [
+  { path: "", component: GroceriesComponent, canActivate: [AuthGuard] },
+  { path: "login", component: LoginComponent }
+];
+
+export const APP_ROUTER_PROVIDERS = [
+  nsProvideRouter(routes, { enableTracing: false }),
+  AuthGuard
+];
